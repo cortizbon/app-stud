fdc4aa34b45460915e313fb702bc99040de9d21b
Carlos Ortiz <carlosortizbon@gmail.com>
2022-06-03 20:05:28 -0500

    cambio en app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from utils import grafica_1, descriptivo, grafica_2, filtro, tasa_aprobacion_temprana, tasa_perdida, importacion_datos, check_password
 
+#autenticación
 password = st.secrets['password'] 
 key1 = st.secrets['key1']
 key2 = st.secrets['key2']
@@ -12,12 +13,14 @@ key2 = st.secrets['key2']
 if check_password() != True:
     st.stop()
     
+#importación de datos
 df = importacion_datos(key1, key2)
 
+#encabezado
 st.title('Facultad de Economía')
 st.header('Comportamiento del desempeño académico de la Facultad de Economía para los años 2020-2021')
 
-
+#definición y filtros
 cols = ['periodo','depto','nom_materia','docente','apellidos_y_nombres','nivel_est']
 nombres = ['Periodo',
             'Departamento',
@@ -33,7 +36,7 @@ for col, nombre in zip(cols, nombres):
         df = filtro(df, col, filtros)
 
 
-#función con gráficas
+#gráficas
 
 st.pyplot(grafica_1(df))
 col1, col2 = st.columns(2)
@@ -42,10 +45,10 @@ col1.write(descriptivo(df))
 col2.pyplot(grafica_2(df))
 
 st.subheader(f'Aprobación temprana: {tasa_aprobacion_temprana(df)}')
-
-
 st.subheader(f'Tasa de no aprobación: {tasa_perdida(df)}')
 st.write()
+
+#notas al pie de pa´gina
 
 st.write('*Al seleccionar la opción "Todos" en una variable junto con otras opciones, el aplicativo ignora las otras opciones y sigue presentando información con la categoría "Todos" para la variable.')
 st.write('**Tasa de aprobación temprana: porcentaje de personas que aprueban una materia antes del tercer corte')
